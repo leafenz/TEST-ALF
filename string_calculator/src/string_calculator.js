00093c3ffb5eb223e1520ded771d676e6b71a5d4
Lea Fenz <if23b161@technikum-wien.at>
2024-05-22 17:37:51 +0200

    test3_green

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -5,6 +5,11 @@ StringCalculator.prototype.find = function(map) {
 		return 'Spaceship lost forever.'
 	}
 	else{
+		let xCount = (map.match(/X/) || []).length;
+        if (xCount === 0) {
+            return 'Spaceship lost forever.';
+        }
+		
 		let xPosition = map.indexOf('X');
     	return `${xPosition},0`;
 	}
