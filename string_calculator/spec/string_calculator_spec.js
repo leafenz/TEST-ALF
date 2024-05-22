d031da3920b6e9415bd9295dbcf31f31871825aa
Lea Fenz <if23b161@technikum-wien.at>
2024-05-22 17:17:44 +0200

    test2_red

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -6,4 +6,9 @@ describe('Spaceship', function() {
 	it('should return "Spaceship lost forever." for an empty string', function() {
 		expect(spaceshipFinder.find('')).toEqual("Spaceship lost forever.");
 	});
+
+	it('should return "Spaceship lost forever." for an empty string', function() {
+		expect(spaceshipFinder.find('....X....')).toEqual("4,0");
+	});
+
 });
